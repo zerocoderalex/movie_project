823394febae5a29a1627896d9dfaa1abbdaa109d
zerocoderalex <alex12449@mail.ru>
2024-11-30 09:50:19 +0500

    add redir

--- MovieProject/films/forms.py
+++ MovieProject/films/forms.py
@@ -1,7 +1,13 @@
 from .models import News_post
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, Textarea
 
 class News_postForm(ModelForm):
 	class Meta:
 		model = News_post
 		fields = ['title', 'short_description', 'text']
+		widgets = {
+			'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Заголовок новости'}),
+			'short_description': TextInput(attrs={'class': 'form-control', 'placeholder': 'Краткое описание новости'}),
+			'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Содержание новости'})
+
+		}
--- MovieProject/films/views.py
+++ MovieProject/films/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render
+from logging import error
+
+from django.shortcuts import render, redirect
 from .models import News_post
 from .forms import News_postForm
 
 # Create your views here.
 def home(request):
     news = News_post.objects.all()
-    return render(request, 'films/index.html', {'films': news})
+    return render(request, 'films/index.html', {'news': news})
 
 def create_news(request):
-    form = News_postForm()
-    return render(request, 'films/add_new_post.html', {'form': form})
+         error=''
+         if request.method == 'POST':
+                 form = News_postForm(request.POST) # Сюда сохранится информация от пользователя.
+                 if form.is_valid():
+                         form.save
+                         return redirect('news_home')
+                 else:
+                         error # "Данные были заполнены некорректно"
+         form = News_postForm()
+         return render(request, 'news/add_new_post.html', {'form': form, 'error': error})
+
+
+
+
+
+
+
+
+
 
